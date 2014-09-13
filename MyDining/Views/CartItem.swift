be3c916b6a4ecdd4edb77c448cc10c1a4008b540
aksondhi <aksondhi@gmail.com>
2014-09-13 14:15:07 -0500

    Added CartItem to views

---
+++ MyDining/Views/CartItem.swift
@@ -0,0 +1,24 @@
+//
+//  CartItem.swift
+//  MyDining
+//
+//  Created by Arun Kumar Sondhi on 9/13/14.
+//  Copyright (c) 2014 Ian McDowell. All rights reserved.
+//
+
+import UIKit
+
+class CartItem: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
